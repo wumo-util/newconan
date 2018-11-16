f8edc8bf71c8669c65d8cb2e5e323a5e4404bf78
wumo <wumo@outlook.com>
2018-11-16 18:24:12 +0800

    fix bugs.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='newconan',
-      version='1.5',
+      version='1.6',
       description='New cmake project based on conan',
       url='https://github.com/wumo/newconan',
       author='wumo',
