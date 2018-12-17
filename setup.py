0ff8696f89e81270ad5029ba9b61af8baea610b2
wumo <wumo@outlook.com>
2018-12-17 18:00:36 +0800

    link libgcc

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='newconan',
-      version='1.21',
+      version='1.22',
       description='New cmake project based on conan',
       url='https://github.com/wumo/newconan',
       author='wumo',
