7d4e79ff2b714d67fed26be710db75d78e90b1cd
wumo <wumo@outlook.com>
2019-01-13 17:42:00 +0800

    test glob

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='newconan',
-      version='1.22',
+      version='1.23',
       description='New cmake project based on conan',
       url='https://github.com/wumo/newconan',
       author='wumo',
