e26d81a3041c495b041804d7ec4c3f174b87048b
wumo <wumo@outlook.com>
2019-02-19 20:06:25 +0800

    fix bug related to .gitignore.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='newconan',
-      version='1.38',
+      version='1.39',
       description='New cmake project based on conan',
       url='https://github.com/wumo/newconan',
       author='wumo',
