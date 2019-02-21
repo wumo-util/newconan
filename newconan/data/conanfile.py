d4cfba827bc4580c6294337448205a94056c512c
wumo <wumo@outlook.com>
2019-02-21 16:05:57 +0800

    linux so.

--- newconan/data/conanfile.py
+++ newconan/data/conanfile.py
@@ -35,8 +35,8 @@ class {project_name}Conan(ConanFile):
   def package(self):
     self.copy("*.h", dst="include", src=f"{self.name}/src")
     self.copy("*.dll", dst="bin", src="bin", keep_path=False)
-    self.copy("*.so", dst="lib", src="lib", keep_path=False)
-    self.copy("*.dylib", dst="lib", src="lib", keep_path=False)
+    self.copy("*.so", dst="bin", src="bin", keep_path=False)
+    self.copy("*.dylib", dst="bin", src="bin", keep_path=False)
     self.copy("*.a", dst="lib", src="lib", keep_path=False)
     self.copy("*.lib", dst="lib", src="lib", keep_path=False)
     self.copy("*", dst=f"resources/{self.name}", src=f"{self.name}/assets/public/{self.name}")
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='newconan',
-      version='1.47',
+      version='1.48',
       description='New cmake project based on conan',
       url='https://github.com/wumo/newconan',
       author='wumo',
