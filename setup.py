9e57d6e3cfe0f3f4b26b1d32026a7f51061a6171
wumo <wumo@outlook.com>
2019-04-25 16:51:17 +0800

    argparse

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='newconan',
-      version='1.62',
+      version='1.63',
       description='New cmake project based on conan',
       url='https://github.com/wumo/newconan',
       author='wumo',
