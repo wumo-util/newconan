a7430a52dc785fa2b22b31ac7f9fecb3be20a516
wumo <wumo@outlook.com>
2019-05-13 14:44:17 +0800

    upgrade conan.cmake

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='newconan',
-      version='1.63',
+      version='1.64',
       description='New cmake project based on conan',
       url='https://github.com/wumo/newconan',
       author='wumo',
