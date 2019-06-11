745337a3e14e881329b5a81c333ad53d9e215351
wumo <wumo@outlook.com>
2019-06-11 14:06:29 +0800

    upgrade c++ 20

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='newconan',
-      version='1.64',
+      version='1.65',
       description='New cmake project based on conan',
       url='https://github.com/wumo/newconan',
       author='wumo',
