5bdcba9a4d476d53c487d7e4a13ab2cf7a836d7e
wumo <wumo@outlook.com>
2019-06-23 18:20:34 +0800

    .clang-format

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='newconan',
-      version='1.65',
+      version='1.66',
       description='New cmake project based on conan',
       url='https://github.com/wumo/newconan',
       author='wumo',
