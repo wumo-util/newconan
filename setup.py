645dcd92a2de55082bd722774da9d532bcce6007
wumo <wumo@outlook.com>
2019-11-08 19:29:46 +0800

    git reset --hard HEAD in .gitlab-ci.yml

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='newconan',
-      version='1.71',
+      version='1.72',
       description='New cmake project based on conan',
       url='https://github.com/wumo/newconan',
       author='wumo',
