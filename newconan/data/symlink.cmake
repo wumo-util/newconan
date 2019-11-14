c75b6c319724826279e7b024f62ca1700ffb35b5
wumo <wumo@outlook.com>
2019-11-14 14:14:43 +0000

    Update MANIFEST.in, newconan/data/symlink.cmake, newconan/data/symlink.py, newconan/newconan.py, newconan/data/LibraryCMakeLists.txt, newconan/data/ExeCMakeLists.txt files

---
+++ newconan/data/symlink.cmake
@@ -0,0 +1,5 @@
+find_package (Python)
+execute_process(COMMAND ${Python_EXECUTABLE} ${CMAKE_CURRENT_LIST_DIR}/symlink.py
+    ${CMAKE_CURRENT_SOURCE_DIR}/assets
+    ${CMAKE_CURRENT_SOURCE_DIR}/assets/public/{project_name}
+    ${CMAKE_CURRENT_BINARY_DIR}/bin/assets)
