8a2180f47141e1e06d377ee4312df83b16ba8e86
wumo <wumo@outlook.com>
2019-11-18 16:10:37 +0800

    copy bin/*

--- newconan/data/conanfile.py
+++ newconan/data/conanfile.py
@@ -34,15 +34,16 @@ class {project_name}Conan(ConanFile):
         self.copy("*", dst="bin/assets/public", src="resources")
     
     def package(self):
+        self.copy("*", dst="bin", src="bin", keep_path=False)
         self.copy("*.h", dst="include", src=f"{self.name}/src")
-        self.copy("*.dll", dst="bin", src="bin", keep_path=False)
-        self.copy("*.so", dst="bin", src="bin", keep_path=False)
         self.copy("*.so", dst="lib", src="lib", keep_path=False)
-        self.copy("*.dylib", dst="bin", src="bin", keep_path=False)
         self.copy("*.a", dst="lib", src="lib", keep_path=False)
         self.copy("*.lib", dst="lib", src="lib", keep_path=False)
-        self.copy("*", dst=f"resources/{self.name}",
-                  src=f"{self.name}/assets/public/{self.name}")
+        # self.copy("*.dll", dst="bin", src="bin", keep_path=False)
+        # self.copy("*.so", dst="bin", src="bin", keep_path=False)
+        # self.copy("*.dylib", dst="bin", src="bin", keep_path=False)
+        # self.copy("*", dst=f"resources/{self.name}",
+        #           src=f"{self.name}/assets/public/{self.name}")
     
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
