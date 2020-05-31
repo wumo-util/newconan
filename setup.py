6d56f838fcbc1e8457cfd55e74473529292fca72
WuMo <wumo@outlook.com>
2020-05-31 14:23:08 +0800

    release plz 2.3. #github actions run on "startsWith"

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", encoding='utf-8', mode='r') as fh:
     long_description = fh.read()
 
 setup(name='newconan',
-      version='2.2',
+      version='2.3',
       description='New cmake project based on conan',
       long_description=long_description,
       long_description_content_type="text/markdown",
